7e6f5c24fdf4fde69636c179491e555e40a8b668
Luis Brayan <luis.bs.9004@gmail.com>
2025-02-28 16:17:23 -0600

    refactor: simplification of calculator

--- app/src/main/java/edu/udb/dsm/desafio_practico_01/CalculatorActivity.kt
+++ app/src/main/java/edu/udb/dsm/desafio_practico_01/CalculatorActivity.kt
@@ -1,12 +1,9 @@
 package edu.udb.dsm.desafio_practico_01
 
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.ArrayAdapter
+import android.widget.CompoundButton
 import android.widget.EditText
-import android.widget.Spinner
+import android.widget.RadioButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -34,10 +31,8 @@ class CalculatorActivity : AppCompatActivity() {
 
         resultOut = findViewById(R.id.cal_result)
 
-        //
-        val operand1In: EditText = findViewById(R.id.cal_operand1)
-        val operand2In: EditText = findViewById(R.id.cal_operand2)
-        operand1In.doAfterTextChanged { text ->
+        // operands
+        findViewById<EditText>(R.id.cal_operand1).doAfterTextChanged { text ->
             val value = text.toString().toDoubleOrNull()
             if (value === null) {
                 Toast.makeText(this, R.string.cal_no_operand1, Toast.LENGTH_SHORT).show()
@@ -46,7 +41,7 @@ class CalculatorActivity : AppCompatActivity() {
                 operate()
             }
         }
-        operand2In.doAfterTextChanged { text ->
+        findViewById<EditText>(R.id.cal_operand2).doAfterTextChanged { text ->
             val value = text.toString().toDoubleOrNull()
             if (value === null) {
                 Toast.makeText(this, R.string.cal_no_operand2, Toast.LENGTH_SHORT).show()
@@ -56,44 +51,39 @@ class CalculatorActivity : AppCompatActivity() {
             }
         }
 
-        //
-        val operatorIn: Spinner = findViewById(R.id.cal_operator)
-        ArrayAdapter.createFromResource(
-            this, R.array.cal_operators, android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            operatorIn.adapter = adapter
-        }
-        operatorIn.onItemSelectedListener = object : OnItemSelectedListener {
-            override fun onItemSelected(
-                adapter: AdapterView<*>,
-                view: View?,
-                position: Int,
-                id: Long
-            ) {
-                operator = position
-                operate()
-            }
+        findViewById<RadioButton>(R.id.cal_add).setOnCheckedChangeListener(this::change)
+        findViewById<RadioButton>(R.id.cal_sub).setOnCheckedChangeListener(this::change)
+        findViewById<RadioButton>(R.id.cal_mul).setOnCheckedChangeListener(this::change)
+        findViewById<RadioButton>(R.id.cal_div).setOnCheckedChangeListener(this::change)
+        findViewById<RadioButton>(R.id.cal_pow).setOnCheckedChangeListener(this::change)
+        findViewById<RadioButton>(R.id.cal_sqrt).setOnCheckedChangeListener(this::change)
+    }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                operator = -1
-                operate()
-            }
-        }
+    // operators
+    private fun change(view: CompoundButton, isChecked: Boolean) {
+        if (view.id == operator) return
+        findViewById<RadioButton>(operator)?.isChecked = false
+        operator = view.id
+        operate()
     }
 
     private fun operate() {
-        if (operator == 3 && operand2 == 0.0) {
+        if (operator == R.id.cal_div && operand2 == 0.0) {
             resultOut?.text = resources.getString(R.string.cal_0division)
+            return
+        }
+        if (operator == R.id.cal_sqrt && operand2 <= 0) {
+            resultOut?.text = resources.getString(R.string.cal_negative_root)
+            return
         }
 
         val result = when (operator) {
-            0 -> operand1.plus(operand2)
-            1 -> operand1.minus(operand2)
-            2 -> operand1.times(operand2)
-            3 -> operand1.div(operand2)
-            4 -> operand1.pow(operand2)
-            5 -> operand1.pow(1 / operand2)
+            R.id.cal_add -> operand1.plus(operand2)
+            R.id.cal_sub -> operand1.minus(operand2)
+            R.id.cal_mul -> operand1.times(operand2)
+            R.id.cal_div -> operand1.div(operand2)
+            R.id.cal_pow -> operand1.pow(operand2)
+            R.id.cal_sqrt -> operand1.pow(1 / operand2)
             else -> 0.0
         }
         resultOut?.text = result.toBigDecimal().toPlainString()
